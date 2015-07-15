77d613468ba1d4763bd51b2e626c19b94c07a141
Taurvi <srimbaksumo@gmail.com>
2015-07-14 18:40:03 -0700

    Added config

--- js/app.js
+++ js/app.js
@@ -1,2 +1,18 @@
 var myApp = angular.module('myApp', ['ngRoute']);
 
+myApp.config(function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'templates/landing.html',
+            controller: 'LandingController'
+        })
+        .when('/content/', {
+            templateUrl: 'templates/content.html',
+            controller: 'ContentController'
+        })
+        .when('/about/', {
+            templateUrl: 'templates/about.html',
+            controller: 'AboutController'
+        })
+})
+
